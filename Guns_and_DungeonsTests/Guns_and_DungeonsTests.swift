f2b15a0af012ef868acacd8a24dd796427f0f14c
Роман Агеев <ageevroma2000@gmail.com>
2019-10-18 12:12:49 +0300

    Initial Commit

---
+++ Guns_and_DungeonsTests/Guns_and_DungeonsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Guns_and_DungeonsTests.swift
+//  Guns_and_DungeonsTests
+//
+//  Created by Роман Агеев on 18.10.2019.
+//  Copyright © 2019 Роман Агеев. All rights reserved.
+//
+
+import XCTest
+@testable import Guns_and_Dungeons
+
+class Guns_and_DungeonsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
