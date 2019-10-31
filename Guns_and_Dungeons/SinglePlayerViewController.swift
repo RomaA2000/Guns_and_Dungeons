34ce499c9d57414291c7e3c298119c0c58a8bc37
Роман Агеев <ageevroma2000@gmail.com>
2019-11-01 00:19:27 +0300

    fix autors

--- Guns_and_Dungeons/SinglePlayerViewController.swift
+++ Guns_and_Dungeons/SinglePlayerViewController.swift
@@ -2,8 +2,7 @@
 //  SinglePlayerViewController.swift
 //  Guns_and_Dungeons
 //
-//  Created by Александр Потапов on 18/10/2019.
-//  Copyright © 2019 точно не Роман Агеев. All rights reserved.
+//  Copyright © 2019 Роман Агеев. All rights reserved.
 //
 
 import Foundation
