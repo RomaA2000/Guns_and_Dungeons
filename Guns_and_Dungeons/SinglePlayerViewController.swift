d2f4d35648a3da6c8607a132b76b46a9862731ed
Роман Агеев <ageevroma2000@gmail.com>
2019-11-01 13:31:28 +0300

    fix

--- Guns_and_Dungeons/SinglePlayerViewController.swift
+++ Guns_and_Dungeons/SinglePlayerViewController.swift
@@ -39,8 +39,6 @@ class PanelInformation {
     }
 }
 
-
-
 class Panel {
     let mainView : UIView
     init(frame: CGRect) {
@@ -57,18 +55,16 @@ class MarnginsInformation {
     var panelWidth: CGFloat = 0
 }
 
-class LevelScroller {
-    var marginsInformation = MarnginsInformation()
-    
-    func setter(number: UInt) {}
-}
-
-class SinglePlayerViewController : UIViewController, UIScrollViewDelegate {
-    let levelPanel: UIScrollView = UIScrollView()
-    var backButton: UIButton = UIButton()
+class LevelPanel : UIScrollView {
     var marginsInformation = MarnginsInformation()
     var aboutLevels : AboutLevels = AboutLevels()
     var panels: [Panel] = []
+}
+
+class SinglePlayerViewController : UIViewController, UIScrollViewDelegate {
+    
+    var levelPanel : LevelPanel = LevelPanel()
+    var backButton: UIButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -82,7 +78,7 @@ class SinglePlayerViewController : UIViewController, UIScrollViewDelegate {
         levelPanel.isScrollEnabled = true
         levelPanel.showsHorizontalScrollIndicator = false
         fillLevelPanel()
-        levelPanel.contentOffset = CGPoint(x: panels[aboutLevels.nowPanel].mainView.center.x - levelPanel.bounds.width / 2, y: 0)
+        levelPanel.contentOffset = CGPoint(x: levelPanel.panels[levelPanel.aboutLevels.nowPanel].mainView.center.x - levelPanel.bounds.width / 2, y: 0)
         levelPanel.delegate = self
     }
     
@@ -91,15 +87,15 @@ class SinglePlayerViewController : UIViewController, UIScrollViewDelegate {
         var targetX: CGFloat = 0;
         var idx : Int = 0
         if (position > levelPanel.bounds.width / 2) {
-            idx = Int((position - levelPanel.bounds.width / 2) / (marginsInformation.panelWidth + marginsInformation.marginMiddle * 2))
-            if (idx >= panels.count - 1) {
-                idx = panels.count - 1
+            idx = Int((position - levelPanel.bounds.width / 2) / (levelPanel.marginsInformation.panelWidth + levelPanel.marginsInformation.marginMiddle * 2))
+            if (idx >= levelPanel.panels.count - 1) {
+                idx = levelPanel.panels.count - 1
             } else {
-                idx += position - panels[idx].mainView.center.x > panels[idx + 1].mainView.center.x - position ? 1 : 0
+                idx += position - levelPanel.panels[idx].mainView.center.x > levelPanel.panels[idx + 1].mainView.center.x - position ? 1 : 0
             }
         }
-        aboutLevels.nowPanel = idx
-        targetX = panels[idx].mainView.center.x - levelPanel.bounds.width / 2
+        levelPanel.aboutLevels.nowPanel = idx
+        targetX = levelPanel.panels[idx].mainView.center.x - levelPanel.bounds.width / 2
         targetContentOffset.pointee.x = targetX
     }
     
@@ -109,33 +105,33 @@ class SinglePlayerViewController : UIViewController, UIScrollViewDelegate {
     }
     
     func addPanel(locationRect: CGRect, information: PanelInformation) -> UIView {
-         panels.append(Panel(frame: locationRect))
+        levelPanel.panels.append(Panel(frame: locationRect))
          for i in 0..<4 {
              let now : PanelButton = PanelButton(frame: CGRect(), num: information.numerator)
-             panels.last!.mainView.posSubviewByRect(subView: now, params: information.cordinates[i])
+            levelPanel.panels.last!.mainView.posSubviewByRect(subView: now, params: information.cordinates[i])
              information.numerator += 1
              now.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
          }
-         return panels.last!.mainView
+        return levelPanel.panels.last!.mainView
      }
     
     func fillLevelPanel() {
         let number = 10
         let panelPart : CGFloat = 0.6
-          var imageRect : CGRect = CGRect(x: 0, y: 0, width: levelPanel.bounds.width * panelPart, height: levelPanel.bounds.height)
+        var imageRect : CGRect = CGRect(x: 0, y: 0, width: levelPanel.bounds.width * panelPart, height: levelPanel.bounds.height)
         let information: PanelInformation = PanelInformation(x_list: [CGFloat(0.30), CGFloat(0.70)], y_list: [CGFloat(0.25), CGFloat(0.75)], k: CGFloat(1), s: CGFloat(0.1))
-        marginsInformation.panelWidth = imageRect.width
-        marginsInformation.marginMiddle = levelPanel.bounds.width * 0.05
-        marginsInformation.marginStart = (levelPanel.bounds.width - imageRect.width) / 22
+        levelPanel.marginsInformation.panelWidth = imageRect.width
+        levelPanel.marginsInformation.marginMiddle = levelPanel.bounds.width * 0.05
+        levelPanel.marginsInformation.marginStart = (levelPanel.bounds.width - imageRect.width) / 2
         for i in 0..<number {
             if (i == 0) {
-                imageRect.origin.x += marginsInformation.marginStart - marginsInformation.marginMiddle
+                imageRect.origin.x += levelPanel.marginsInformation.marginStart - levelPanel.marginsInformation.marginMiddle
             }
-            imageRect.origin.x += marginsInformation.marginMiddle
+            imageRect.origin.x += levelPanel.marginsInformation.marginMiddle
             levelPanel.addSubview(addPanel(locationRect: imageRect, information: information))
-            imageRect.origin.x += imageRect.width + marginsInformation.marginMiddle
+            imageRect.origin.x += imageRect.width + levelPanel.marginsInformation.marginMiddle
         }
-        levelPanel.contentSize = CGSize(width: marginsInformation.marginStart * 2 + marginsInformation.marginMiddle * CGFloat(2 * (number - 1)) + imageRect.width * CGFloat(number), height: imageRect.height)
+        levelPanel.contentSize = CGSize(width: levelPanel.marginsInformation.marginStart * 2 + levelPanel.marginsInformation.marginMiddle * CGFloat(2 * (number - 1)) + imageRect.width * CGFloat(number), height: imageRect.height)
     }
 
     @objc func toMenuScreen() {
