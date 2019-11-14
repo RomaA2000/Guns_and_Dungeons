008f1c504ccbcd2e3161842b4a4235b7ecaff757
Роман Агеев <ageevroma2000@gmail.com>
2019-11-15 00:39:09 +0300

    fix

--- Guns_and_Dungeons/GameScreen/Joystic.swift
+++ Guns_and_Dungeons/GameScreen/Joystic.swift
@@ -1,3 +1,10 @@
+//
+//  Wall.swift
+//  Guns_and_Dungeons
+//
+//  Created by Роман Агеев on 14.11.2019.
+//  Copyright © 2019 Роман Агеев. All rights reserved.
+//
 import SpriteKit
 
 public typealias AnalogJoystick = TLAnalogJoystick
