feddf4aa693d55e17c9af89e468460b2437eb526
Роман Агеев <ageevroma2000@gmail.com>
2019-12-27 01:46:29 +0300

    shop

--- Guns_and_Dungeons/Screens/ShopScreen/ShopViewController.swift
+++ Guns_and_Dungeons/Screens/ShopScreen/ShopViewController.swift
@@ -105,15 +105,16 @@ class ShopViewController : UIViewController {
 
         let netCollector = NetCollector(gunsSelector: self.generateUpdater(number: 0),
                                         basesSelector: self.generateUpdater(number: 1))
+        let gunUrl = URL(string:"https://psv4.userapi.com/c856336/u251664583/docs/d12/f1b149d62a0f/rocket1.png?extra=ngAxzY5aFg6SDD_mnOuCQ61jWty02RGY3wXfGAFsXuELjBH4WYqDPbucoO4ErljAh93fgVjur9tyhw_QY2BtD5Uk5zgaTB-6yGUXOEuNUmrOJIVHt-OVVmUNLqhEdiRMDjiExtFkOsdTZtzyXhMAlSA")!
         let gunsUrl: Array<(URL, Int)> =
-            [(URL(string: "https://upload.wikimedia.org/wikipedia/commons/a/af/%D0%9A%D0%9F%D0%A1%D0%A1.png")!, 6),
-            (URL(string: "https://upload.wikimedia.org/wikipedia/commons/a/af/%D0%9A%D0%9F%D0%A1%D0%A1.png")!, 7),
-            (URL(string: "https://upload.wikimedia.org/wikipedia/commons/a/af/%D0%9A%D0%9F%D0%A1%D0%A1.png")!, 8)]
-
+            [(gunUrl, 6),
+            (gunUrl, 7),
+            (gunUrl, 8)]
+        let baseUrl = URL(string: "https://psv4.userapi.com/c856416/u251664583/docs/d3/54c05b31018f/en1.png?extra=R6rsMIL7FSyMa6Q7QfTEGqxG6xkOl_I5eooGYkeWaZHyXvLJ4VJc_diCaetwvAQyJrMc5LMdUi-3oAQhR7nPQDSh0Htgx4BbUCVSiavbKOIDHRhae7hGOXxyr02fa9EObI7CGTRhu42e-OhtrIGLHxQ")!
         let basesUrl: Array<(URL, Int)> =
-            [(URL(string: "https://psv4.userapi.com/c856416/u251664583/docs/d3/54c05b31018f/en1.png?extra=R6rsMIL7FSyMa6Q7QfTEGqxG6xkOl_I5eooGYkeWaZHyXvLJ4VJc_diCaetwvAQyJrMc5LMdUi-3oAQhR7nPQDSh0Htgx4BbUCVSiavbKOIDHRhae7hGOXxyr02fa9EObI7CGTRhu42e-OhtrIGLHxQ")!, 6),
-             (URL(string: "https://psv4.userapi.com/c856416/u251664583/docs/d3/54c05b31018f/en1.png?extra=R6rsMIL7FSyMa6Q7QfTEGqxG6xkOl_I5eooGYkeWaZHyXvLJ4VJc_diCaetwvAQyJrMc5LMdUi-3oAQhR7nPQDSh0Htgx4BbUCVSiavbKOIDHRhae7hGOXxyr02fa9EObI7CGTRhu42e-OhtrIGLHxQ")!, 7),
-             (URL(string: "https://psv4.userapi.com/c856416/u251664583/docs/d3/54c05b31018f/en1.png?extra=R6rsMIL7FSyMa6Q7QfTEGqxG6xkOl_I5eooGYkeWaZHyXvLJ4VJc_diCaetwvAQyJrMc5LMdUi-3oAQhR7nPQDSh0Htgx4BbUCVSiavbKOIDHRhae7hGOXxyr02fa9EObI7CGTRhu42e-OhtrIGLHxQ")!, 8)]
+            [(baseUrl, 6),
+             (baseUrl, 7),
+             (baseUrl, 8)]
         netCollector.startLoadingData(urlsGunsPos: gunsUrl, urlsBasesPos: basesUrl)
     }
 }
